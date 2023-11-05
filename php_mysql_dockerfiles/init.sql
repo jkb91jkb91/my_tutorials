ae9ba1a190e6ade0dd7ac57bdbc37c81f4344e7f
Kuba <kuba>
2023-11-05 16:10:03 +0100

    working docker-compose

---
+++ php_mysql_dockerfiles/init.sql
@@ -0,0 +1,14 @@
+
+mysql -u root -proot
+
+Use test1;
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(250) NOT NULL,
+  email VARCHAR(250) UNIQUE,
+  phone VARCHAR(100),
+  bgroup VARCHAR(100)
+);
+
+INSERT INTO users (name, email) VALUES ('Test User', 'test@example.com');
