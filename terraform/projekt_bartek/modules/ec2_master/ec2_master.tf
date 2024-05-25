c1b8f7378f791bbfbfed4f5db22c1ab78fbb101e
jakub <jakub.g26101991@gmail.com>
2024-05-25 17:41:46 +0200

    added ansible in terraform

--- terraform/projekt_bartek/modules/ec2_master/ec2_master.tf
+++ terraform/projekt_bartek/modules/ec2_master/ec2_master.tf
@@ -6,6 +6,9 @@ locals {
     Owner_2 = "bartek"
     Project = "devops_project_aws"
   }
+  ssh_user  = "ubuntu"
+  key_name  = "jenkins"
+
 }
 
 resource "aws_security_group" "web_sg" {
@@ -20,16 +23,59 @@ resource "aws_security_group" "web_sg" {
     protocol    = var.security_group.protocol
     cidr_blocks = var.security_group.cidr_blocks
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.example.private_key_pem
+  filename = "/tmp/deployer-key.pem"
+  file_permission = "0400"
 }
 
 resource "aws_instance" "instance" {
-  count         = var.ec2_count
-  ami           = var.ami_us_east_2
-  instance_type = var.ec2_instance_type
-  tags          = local.common_tags
-  subnet_id     = var.public_subnet_ids[0]
-  
+  #count                  = var.ec2_count
+  ami                    = var.ami_us_east_2
+  instance_type          = var.ec2_instance_type
+  tags                   = local.common_tags
+  subnet_id              = var.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo echo 'Wait for ssh creation'"
+    ]
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = tls_private_key.example.private_key_pem
+      host        = self.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+      cd ANSIBLE/master
+      sed -i 's/IP/${self.public_ip}/g' hosts
+      ansible-playbook -i hosts --private-key ${local_file.private_key.filename} --ssh-extra-args="-o StrictHostKeyChecking=no" provisioning.yaml -vvv
+    EOT
+  }
 }
 
 # resource "local_file" "TF_key" {
