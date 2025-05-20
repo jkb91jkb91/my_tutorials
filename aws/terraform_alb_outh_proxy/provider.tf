d3a100f06624af772477d98e8b8da717366015bc
Jakub <jakub.g26101991@gmail.com>
2025-05-20 12:44:23 +0200

    oauth_proxy on EC2

---
+++ aws/terraform_alb_outh_proxy/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "PUT_YOUR_ACCESS_KEY"
+  secret_key = "PUT_YOUR_SECRET_KEY"
+}
